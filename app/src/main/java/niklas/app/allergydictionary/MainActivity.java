86f51d4f3fa463e73b57f7c826caeeb17982cf96
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2018-02-06 14:35:23 +0100

    Show Dialog only when first time used, Version 3

--- app/src/main/java/niklas/app/allergydictionary/MainActivity.java
+++ app/src/main/java/niklas/app/allergydictionary/MainActivity.java
@@ -3,6 +3,7 @@ package niklas.app.allergydictionary;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,14 +17,13 @@ import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final String PREFS_NAME = "AllergyDictFile";
 
     // TODO Datenbank
-    //TODO Neue APK + Bilder in App Store
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         DataStorage.setUpItemsByLanguage();
@@ -42,10 +42,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        showSecurityDialog();
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        boolean showDialog = settings.getBoolean("showDialog", true);
+
+        if(showDialog) {
+            showSecurityDialog();
+        }
 
         itemsAdapter.notifyDataSetChanged();
     }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("showDialog", false);
+
+        // Commit the edits!
+        editor.apply();
+    }
+
 
     private void showSecurityDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
